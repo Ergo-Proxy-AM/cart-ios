5c64847b406b4edd73b96d46112c92cdd1f643e5
axelmitura <mitura.axel@gmail.com>
2022-05-22 17:35:30 +0200

    Initial Commit

---
+++ Cart/CartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartApp.swift
+//  Cart
+//
+//  Created by Axel Mitura on 22/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
